cd3e352b9f1b338bbf60edc18e6d71b840275670
Lastknight12 <vovaua27@gmail.com>
2025-04-07 15:52:57 +0300

    removed a non-functional feature

--- server/src/controllers/message.controller.js
+++ server/src/controllers/message.controller.js
@@ -1,22 +1,7 @@
-import User from "../models/user.models.js";
 import Message from "../models/message.models.js";
 
 import cloudinary from "../lib/cloudinary.js";
 import { getReceiverSocketId, io } from "../lib/socket.js";
-
-export const getUsersForSidebar = async (req, res) => {
-  try {
-    const loggedInUserId = req.user._id;
-    const filteredUsers = await User.find({
-      _id: { $ne: loggedInUserId },
-    }).select("-password");
-
-    res.status(200).json(filteredUsers);
-  } catch (error) {
-    console.error("Error in getUsersForSidebar: ", error.message);
-    res.status(500).json({ error: "Internal server error" });
-  }
-};
 
 export const getMessages = async (req, res) => {
   try {
