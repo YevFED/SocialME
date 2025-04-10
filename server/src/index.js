e228794bb9f67f6fe2ed9031e9d8c846ae63bb34
YevFED <yevheniidev2@gmail.com>
2025-04-10 20:00:01 +0300

    Vercel rewrite

--- server/src/index.js
+++ server/src/index.js
@@ -10,13 +10,14 @@ dotenv.config();
 
 const PORT = process.env.PORT;
 
-app.use(cors());
+// app.use(cors());
 
-// app.use(
-//   cors({
-//     origin: ["http://localhost:5173/", "http://yourapp.com/"],
-//   })
-// );
+app.use(
+  cors({
+    origin: ["http://localhost:5173/", "https://social-me-navy.vercel.app/"],
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "50mb" }));
 
